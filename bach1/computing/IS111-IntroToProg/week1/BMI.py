6549358515ee77ee1ff3a3650031cf41767e09a4
Gabriel Ong <117062305+gongahkia@users.noreply.github.com>
2023-08-18 12:21:54 +0800

    Update BMI.py

--- bach1/computing/IS111-IntroToProg/week1/BMI.py
+++ bach1/computing/IS111-IntroToProg/week1/BMI.py
@@ -1,5 +1,6 @@
-# assuming no incorrect input
-user_weight = float(input("What's your weight (in kg)? "))
-user_height = float(input("What's your height (in m)? "))
-final_BMI = user_weight / user_height ** 2
-print(f"Your BMI is {final_BMI}")
+try:
+  user_weight = float(input("What's your weight (in kg)? "))user_height = float(input("What's your height (in m)? "))
+  final_BMI = user_weight / user_height ** 2
+  print(f"Your BMI is {final_BMI}")
+except:
+  print("Invalid input detected. Please input a valid weight or height.")
