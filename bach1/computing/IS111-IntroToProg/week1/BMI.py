1bba58e1776024dfcfad2d1ac6856f2796d9098e
gongahkia <angryapplegravy@gmail.com>
2023-08-18 12:30:34 +0800

    shiok

--- bach1/computing/IS111-IntroToProg/week1/BMI.py
+++ bach1/computing/IS111-IntroToProg/week1/BMI.py
@@ -1,6 +1,7 @@
 try:
-  user_weight = float(input("What's your weight (in kg)? "))user_height = float(input("What's your height (in m)? "))
-  final_BMI = user_weight / user_height ** 2
-  print(f"Your BMI is {final_BMI}")
+  user_weight = float(input("What's your weight (in kg)? "))
+  user_height = float(input("What's your height (in m)? "))
+  user_BMI = user_weight / user_height ** 2
+  print(f"Your BMI is {user_BMI}")
 except:
   print("Invalid input detected. Please input a valid weight or height.")
