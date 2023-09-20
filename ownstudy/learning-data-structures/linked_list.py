23e3b70fe18ca71616b8bf39ad325908f05bff64
gongahkia <angryapplegravy@gmail.com>
2023-09-21 01:17:47 +0800

    default filler message

---
+++ ownstudy/learning-data-structures/linked_list.py
@@ -0,0 +1,12 @@
+# Linked List
+    # - linear data structure
+    # - values not stored in a contiguous array of bytes, but are linked together using pointers
+    # - each node consists of data and a pointer pointing to the next node
+
+class Linked_list:
+
+    def __init__(self, content=[()]):
+        self.linked_list:list(tuple) = content
+        print("A linked list is instantiated.")
+
+# FUA: Add more methods here and google for understanding
